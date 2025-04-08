e7d1e7c5e7c69ddb849ec75c25c93da64be4a80f
Leandro Simplicio <nightmare.maggot@gmail.com>
2025-04-07 23:03:13 -0300

    menu responsivo

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,6 +1,17 @@
 let btnMenu = document.getElementById("btn_open");
 let menu = document.getElementById("hamburger-button");
+let overlay = document.getElementById("overlay__menu");
+
 
 btnMenu.addEventListener("click", () => {
   menu.classList.add("open");
 });
+
+menu.addEventListener("click", () => {
+  menu.classList.remove("open");
+});
+
+overlay.addEventListener("click", () => {
+  menu.classList.remove("open");
+});
+
